1702c95ab2421db13c6a76ed2e77a1c74980b270
Abdul Ghani <ghanisheikh26@gmail.com>
2025-08-08 18:01:26 +0530

    added env file

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose=require('mongoose');
-require("dotenv").config();
+if(process.env.NODE_ENV!=="production"){
+    require("dotenv").config();
+}
 mongoose.connect(process.env.MONGO_URI);
 
 const userSchema=mongoose.Schema({
